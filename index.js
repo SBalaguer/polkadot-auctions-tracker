e6dfea4ba74d435e504c54a5a1f710f28b9f55ea
Santi Balaguer <santibalaguer@Santis-MacBook-Pro.local>
2022-11-17 14:31:19 +0100

    added date parser with avg block production calculation

--- index.js
+++ index.js
@@ -36,6 +36,10 @@ async function main () {
     // Get all constants
     const {durationEndingPeriod} = await getConstants();
 
+    // Get averageBlockTimes -> this will help calculate target dates
+
+    const {avgBlockTime, lastBlockNumber, lastBlockTimestamp} = await calculateAvgBlockTime();
+
     // Get Scheduled auctions
     const scheduledAuctions = await getScheduledActions();
 
@@ -70,20 +74,30 @@ async function main () {
         for (let i=0; i<=iterations; i++){
             if (!i){
                 const startAuction = scheduledAuction.blockExecution
+                const ending_period_start_block = startAuction + convertToNumber(scheduledAuction.call.Value.args.duration)
+                const auction_end =  startAuction + convertToNumber(scheduledAuction.call.Value.args.duration) + durationEndingPeriod
                 const newAuction = {
                     "starting_period_block": startAuction,
-                    "ending_period_start_block": startAuction + convertToNumber(scheduledAuction.call.Value.args.duration),
-                    "auction_end": startAuction + convertToNumber(scheduledAuction.call.Value.args.duration) + durationEndingPeriod,
-                    "first_lease_period": convertToNumber(scheduledAuction.call.Value.args.lease_period_index)
+                    ending_period_start_block,
+                    auction_end,
+                    "first_lease_period": convertToNumber(scheduledAuction.call.Value.args.lease_period_index),
+                    "starting_period_block_date": calculateTargetDate(lastBlockTimestamp, lastBlockNumber, startAuction, avgBlockTime),
+                    "ending_period_start_block": calculateTargetDate(lastBlockTimestamp, lastBlockNumber, ending_period_start_block, avgBlockTime),
+                    "auction_end": calculateTargetDate(lastBlockTimestamp, lastBlockNumber, auction_end, avgBlockTime),
                 }
                 parsedListOfAuctions.push(newAuction)
             }else{
                 const startAuction = parsedListOfAuctions[parsedListOfAuctions.length-1].starting_period_block + convertToNumber(scheduledAuction.maybePeriodic[0])
+                const ending_period_start_block = startAuction + convertToNumber(scheduledAuction.call.Value.args.duration);
+                const auction_end = startAuction + convertToNumber(scheduledAuction.call.Value.args.duration) + durationEndingPeriod;
                 const newAuction = {
                     "starting_period_block": startAuction,
-                    "ending_period_start_block": startAuction + convertToNumber(scheduledAuction.call.Value.args.duration),
-                    "auction_end": startAuction + convertToNumber(scheduledAuction.call.Value.args.duration) + durationEndingPeriod,
-                    "first_lease_period": convertToNumber(scheduledAuction.call.Value.args.lease_period_index)
+                    ending_period_start_block,
+                    auction_end,
+                    "first_lease_period": convertToNumber(scheduledAuction.call.Value.args.lease_period_index),
+                    "starting_period_block_date": calculateTargetDate(lastBlockTimestamp, lastBlockNumber, startAuction, avgBlockTime),
+                    "ending_period_start_block_date": calculateTargetDate(lastBlockTimestamp, lastBlockNumber, ending_period_start_block, avgBlockTime),
+                    "auction_end_date": calculateTargetDate(lastBlockTimestamp, lastBlockNumber, auction_end, avgBlockTime),
                 }
                 parsedListOfAuctions.push(newAuction)
             }
@@ -153,8 +167,34 @@ const getCurrentAuction = async () => {
     return {auctionsCounter, isAuctionActive, activeAuctionInformation, currentAuction, currentAuctionEndStartBlock, currentAuctionLP}
 }
 
+const calculateAvgBlockTime = async () => {
+
+    const FIRST_TIMESTAMP = 1590507378000
+
+    const lastBlockHeader = await api.rpc.chain.getHeader();
+    const lastBlockNumber = convertToNumber(lastBlockHeader.number.toHuman());
+    const lastBlockHash = lastBlockHeader.hash;
+    const apiAt = await api.at(lastBlockHash)
+    const lastBlockTimestamp = convertToNumber((await apiAt.query.timestamp.now()).toHuman());
+
+    const timeElapsed = (lastBlockTimestamp - FIRST_TIMESTAMP)/1000
+    const avgBlockTime = timeElapsed / (lastBlockNumber - 1)
+
+    return {avgBlockTime, lastBlockNumber, lastBlockTimestamp}
+    
+}
+
+const calculateTargetDate = (t, b1, b2, avg) => {
+    //given the timemstamp and height of b1, and the avg block time production, this function returns the potential timestamp of b2.
+    const targetTimestamp = t + ((b2-b1) * avg * 1000);
+    return new Date(targetTimestamp)
+}
+
+
+
 const convertToNumber = (input) =>{
     return Number(input.split(",").join(""));
 }
 
+// calculateAvgBlockTime().catch(console.error).finally(() => process.exit());
 main().catch(console.error).finally(() => process.exit());
